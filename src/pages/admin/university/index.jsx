99e2fe495ea9c20729cc605d53385c9e894e46ef
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-09 17:03:21 +0530

    manage

--- src/pages/admin/university/index.jsx
+++ src/pages/admin/university/index.jsx
@@ -82,7 +82,7 @@ export default function Index() {
                 {loading ? (
                     <Loader />
                 ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
                         {data &&
                             data?.universities?.map((item, index) => (
                                 <div
