a12415f79fec5be942ee88a290b23578e43e5731
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-10 12:03:37 +0530

    manage about  us page

--- src/pages/about-us/WhyChooseUs.jsx
+++ src/pages/about-us/WhyChooseUs.jsx
@@ -7,7 +7,6 @@ const WhyChooseUs = () => {
   return (
     <section className="py-16 sm:py-24 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-20">
           
           <div className="space-y-12 relative">
