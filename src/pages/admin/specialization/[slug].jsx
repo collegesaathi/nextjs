3114d8f38c2b1f3c894b4672ed4653dbd767dd7b
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-17 13:29:10 +0530

    manages daat

--- src/pages/admin/specialization/[slug].jsx
+++ src/pages/admin/specialization/[slug].jsx
@@ -3,10 +3,8 @@ function Details() {
 
 
     return (<>
-        <AdminLayout page={"university"}>
             <div className="py-4 md:py-8 ">
             </div>
-        </AdminLayout>
     </>);
 }
 
