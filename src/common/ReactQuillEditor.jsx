f2b7b807d5bd8bdd716542eec439392f2312b215
collegesaathi <nemrajcollegesathi.com@gmail.com>
2026-01-05 12:12:06 +0530

    manages Data

--- src/common/ReactQuillEditor.jsx
+++ src/common/ReactQuillEditor.jsx
@@ -1,55 +1,54 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import React from "react";
 import "react-quill/dist/quill.snow.css";
 
 const ReactQuill = dynamic(() => import("react-quill"), {
   ssr: false,
 });
 
-const modules = {
-  toolbar: [
-    [{ header: [1, 2, 3, false] }],
-    ["bold", "italic", "underline"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    [{ script: "sub" }, { script: "super" }],
-    ["code-block"],
-    ["link", "image"],
-  ],
-};
+export default function ReactQuillEditor({
+  label,
+  desc = "",
+  handleBioChange,
+}) {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }],
+      ["code-block"],
+      ["link", "image"],
+    ],
+  };
 
-const formats = [
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "list",
-  "bullet",
-  "script",
-  "code-block",
-  "link",
-  "image",
-];
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "list",
+    "bullet",
+    "script",
+    "code-block",
+    "link",
+    "image",
+  ];
 
-const ReactQuillEditor = ({ label, desc = "", handleBioChange }) => {
   return (
     <div className="mb-3 mt-3">
       <label className="block text-[#FF1B1B] font-semibold mb-2">
         {label}
       </label>
 
-      <div className="border border-gray-300 bg-white rounded-md">
-        <ReactQuill
-          theme="snow"
-          value={desc || ""}
-          onChange={handleBioChange}
-          modules={modules}
-          formats={formats}
-        />
-      </div>
+      <ReactQuill
+        theme="snow"
+        value={desc}
+        onChange={handleBioChange}
+        modules={modules}
+        formats={formats}
+      />
     </div>
   );
-};
-
-export default ReactQuillEditor;
+}
