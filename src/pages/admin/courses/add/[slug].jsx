2d180841d2af21557881058efb51af387acad9d5
collegesaathi <nemrajcollegesathi.com@gmail.com>
2026-01-10 17:31:32 +0530

    manages pro insight page

--- src/pages/admin/courses/add/[slug].jsx
+++ src/pages/admin/courses/add/[slug].jsx
@@ -912,9 +912,6 @@ indianArray.forEach((item, index) => {
                             <div>
                                 <label className="flex justify-between text-[#FF1B1B] font-medium mb-1">
                                     Name{" "}
-                                    <span className="text-sm text-gray-500">
-                                        ({formData.approvals_name?.length}/50)
-                                    </span>
                                 </label>
                                 <input
                                     type="text"
@@ -1101,7 +1098,7 @@ indianArray.forEach((item, index) => {
                                     name="partnersname"
                                     value={formData.partnersname}
                                     onChange={(e) => {
-                                        if (e.target.value.length <= 50) handleChange(e);
+                                         handleChange(e);
                                     }}
                                     placeholder="Enter partners name"
                                     className="w-full p-3 rounded-md bg-gray-100 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#CECECE]"
